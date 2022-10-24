552dc7aa7f72b2713db27496a1a253b6521abe1e
Firza Naufal Alift <firzanaufal@std.unissula.ac.id>
2022-10-24 19:57:46 +0700

    Update activity

---
+++ app/src/main/java/com/example/appcoba/Scroll.kt
@@ -0,0 +1,11 @@
+package com.example.appcoba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scroll : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll)
+    }
+}
